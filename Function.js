49afe98f35125377911771499c6e24ad284652ef
a-Suvani <132464780+a-Suvani@users.noreply.github.com>
2023-05-07 17:19:31 +0545

    Add files via upload

---
+++ Function.js
@@ -0,0 +1,13 @@
+// Write a function to rake 5 number sand to find the mean of the numbers in Javascript
+
+let num1 = prompt("Enter the first num");
+let num2 = prompt("Enter the first num");
+let num3 = prompt("Enter the first num");
+let num4 = prompt("Enter the first num");
+let num5 = prompt("Enter the first num");
+
+function mean() {
+  let mean = (num1 + num2 + num3 + num4 + num5) / 5;
+
+}
+console.log(mean);
